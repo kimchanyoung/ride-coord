e8633acb924b594334be2869998ea5679dc44ce6
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-11 15:50:39 -0500

    Make vehicle creation

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,3 +1,28 @@
 class VehiclesController < ApplicationController
+  def index
+  end
 
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    if @vehicle.save
+      redirect_to event_path(Event.find_by(id: params[:event_id]))
+    else
+      render 'vehicles/new'
+    end
+  end
+
+  def show
+    @vehicle = Vehicle.find_by(id: params[:id])
+  end
+
+  private
+
+  def vehicle_params
+    known_attrs = { event_id: params[:event_id] }
+    params.require(:vehicle).permit(:name).merge(known_attrs)
+  end
 end
