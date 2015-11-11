cc1b37e7ce63cec33ce6055e4db5f220e6fa1b38
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-11 12:42:19 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :events, only: [:index, :show, :new, :create] do
+    resources :vehicles, only: [:index, :show, :new, :create] do
+      resources :users, only: [:index]
+    end
+    resources :users, only: [:index, :show, :new, :create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
