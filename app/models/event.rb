ae6c8c08fceef37d10f9865e82dc840c1feedbfb
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-11 15:50:58 -0500

    Make events show routes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   has_many :vehicles, foreign_key: 'event_id'
   has_many :users, foreign_key: 'event_id'
+
+  def rideless_users
+    users.where(vehicle_id: nil)
+  end
 end
