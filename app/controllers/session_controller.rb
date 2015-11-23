832b5784df8b396a7d2f541ce6792d01ff00ea74
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-11-23 14:10:15 -0500

    Create sessions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,15 @@
 class SessionController < ApplicationController
+  def new
 
+  end
+
+  def create
+    user = User.find_by(username: session_params[:username])
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
 end
